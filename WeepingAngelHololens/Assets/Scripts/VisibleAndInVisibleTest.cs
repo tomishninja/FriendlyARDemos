1d4929f8b6170a0f790e8e02f48a9f96f33c113d
Jean DJADJA <jeanlemirates@gmail.com>
2021-05-26 14:05:39 +0930

    initalCommit

---
+++ WeepingAngelHololens/Assets/Scripts/VisibleAndInVisibleTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleAndInVisibleTest : MonoBehaviour
+{
+
+    private void OnBecameVisible()
+    {
+        Debug.Log("visible");
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("invisible");
+    }
+}
---
+++ WeepingAngelHololens/Assets/Scripts/WeepingAngleBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class WeepingAngleBehaviour : MonoBehaviour
+{
+    public Vector3 StartingDistanceFromUser = new Vector3(0,0,10);
+
+    public float AmountToMoveEachSecond = 1;
+
+    private bool isFacingUser = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // we have the assumption that main camera starts at 0,0,0 in unity;
+        gameObject.transform.position = StartingDistanceFromUser;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFacingUser)
+        {
+            if (Vector3.Distance(gameObject.transform.position, Camera.main.transform.position) > 0.5)
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Camera.main.transform.position, AmountToMoveEachSecond / 60);
+            gameObject.transform.LookAt(Camera.main.transform.position);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        isFacingUser = false;
+    }
+
+    private void OnBecameVisible()
+    {
+        isFacingUser = true;
+    }
+}
