e587c89462d6ba7a54759c845d8b3a4f715ba034
Thomas Clarke <tomishninja@gmail.com>
2021-05-27 18:04:55 +0930

    working with model

--- WeepingAngelHololens/Assets/Scripts/WeepingAngleBehaviour.cs
+++ WeepingAngelHololens/Assets/Scripts/WeepingAngleBehaviour.cs
@@ -6,13 +6,20 @@ public class WeepingAngleBehaviour : MonoBehaviour
 
     public float AmountToMoveEachSecond = 1;
 
+    public GameObject gameObjectToMove;
+
     private bool isFacingUser = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (gameObjectToMove == null)
+        {
+            this.gameObjectToMove = this.gameObject;
+        }
+
         // we have the assumption that main camera starts at 0,0,0 in unity;
-        gameObject.transform.position = StartingDistanceFromUser;
+        this.gameObjectToMove.transform.position = StartingDistanceFromUser;
     }
 
     // Update is called once per frame
@@ -20,19 +27,25 @@ public class WeepingAngleBehaviour : MonoBehaviour
     {
         if (!isFacingUser)
         {
-            if (Vector3.Distance(gameObject.transform.position, Camera.main.transform.position) > 0.5)
-                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Camera.main.transform.position, AmountToMoveEachSecond / 60);
-            gameObject.transform.LookAt(Camera.main.transform.position);
+            if (Vector3.Distance(gameObjectToMove.transform.position, Camera.main.transform.position) > 0.5)
+            {
+                gameObjectToMove.transform.position = Vector3.MoveTowards(gameObject.transform.position, Camera.main.transform.position, AmountToMoveEachSecond / 60);
+                gameObjectToMove.transform.position = new Vector3(gameObjectToMove.transform.position.x, StartingDistanceFromUser.y, gameObjectToMove.transform.position.z);
+            }
+                
+            gameObjectToMove.transform.LookAt(Camera.main.transform.position);
         }
     }
 
     private void OnBecameInvisible()
     {
         isFacingUser = false;
+        Debug.Log("INvisible");
     }
 
     private void OnBecameVisible()
     {
         isFacingUser = true;
+        Debug.Log("visible");
     }
 }
